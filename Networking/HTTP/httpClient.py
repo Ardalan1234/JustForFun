145e86e6c1713ac8479c999a0bce25d2be0fe998
ardalan <aomidrad@gmail.com>
2023-09-02 19:01:53 +0330

    http server

---
+++ Networking/HTTP/httpClient.py
@@ -0,0 +1,20 @@
+import http.client
+
+HOST = '192.168.1.5'
+PORT = 8000
+
+# Create an HTTP connection
+conn = http.client.HTTPConnection(HOST, PORT)
+
+# Send an HTTP GET request
+conn.request("GET", "/")
+
+# Get the response
+response = conn.getresponse()
+
+# Print the response status code and body
+print("Response Status:", response.status)
+print("Response Body:", response.read().decode())
+
+# Close the connection
+conn.close()
---
+++ Networking/HTTP/httpServer.py
@@ -0,0 +1,32 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+HOST = '192.168.1.5'
+PORT = 8000
+
+
+# Define the request handler class
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Set the response status code
+        self.send_response(200)
+
+        # Set the response headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Set the response content
+        response = "Hello, World!"
+
+        # Send the response content
+        self.wfile.write(response.encode())
+
+
+# Set the server address and port
+server_address = (HOST, PORT)
+
+# Create an HTTP server with the request handler class
+http_server = HTTPServer(server_address, HTTPRequestHandler)
+
+# Start the server
+print(f'Server running on port {PORT}...')
+http_server.serve_forever()
